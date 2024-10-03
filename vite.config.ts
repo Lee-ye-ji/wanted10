bccd52ec79bb397e3b2b43a44ed6de5596837d07
이예지 <yeji@iyejiui-MacBookPro.local>
2024-10-03 20:57:51 +0900

    Feat: 다시 시도

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 여기에서 '/wanted10/'로 설정
+  base: "/wanted10/", // 여기에서 '/wanted10/'로 설정
 });
