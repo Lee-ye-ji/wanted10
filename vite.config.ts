338194b4634ec9550fd7cc0818d64e70b77b36fa
이예지 <yeji@iyejiui-MacBookPro.local>
2024-10-03 20:19:15 +0900

    Chore: 경로 확인하기

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/wanted10/", // 여기에서 '/wanted10/'로 설정
+  base: "/", // 여기에서 '/wanted10/'로 설정
 });
